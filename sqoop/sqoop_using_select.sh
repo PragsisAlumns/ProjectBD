7e99bb091308bc89ca03dd70d7744c9cbe8214b3
training <gregjim11@gmail.com>
2015-04-14 01:36:24 -0700

    Removed idvisor

--- sqoop/sqoop_using_select.sh
+++ sqoop/sqoop_using_select.sh
@@ -13,7 +13,7 @@
 #IMPORTING TABLES.
 
 sqoop import --connect jdbc:mysql://dev.loudacre.com/test --username training --password training \
---query 'SELECT idlink_va, idvisit, idvisitor, idaction_url, idaction_url_ref FROM piwik_log_link_visit_action WHERE $CONDITIONS' \
+--query 'SELECT idlink_va, idvisit, idaction_url, idaction_url_ref FROM piwik_log_link_visit_action WHERE $CONDITIONS' \
 --split-by idlink_va \
 --target-dir /piwik/log_link_visit_action \
 --null-non-string '\\N'
