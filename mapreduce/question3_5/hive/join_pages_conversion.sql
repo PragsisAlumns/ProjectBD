7802edf96f6f745c80f02be49b3e4cff2a0c008d
Gregorio <gregorio.jimenezmesa@telefonica.com>
2015-04-30 10:11:48 +0200

    Update join_pages_conversion.sql

--- mapreduce/question3_5/hive/join_pages_conversion.sql
+++ mapreduce/question3_5/hive/join_pages_conversion.sql
@@ -1,9 +1,5 @@
 
-#hadoop jar target/convert-page-1.0-SNAPSHOT.jar com.piwik.totalcounter.TotalCounterPageDriver /piwik/paths_by_visit /piwik/totalcountpage
 
-#########################
-# piwik_total_count_page#
-#########################
 drop table piwik_total_count_page;
 CREATE EXTERNAL TABLE piwik_total_count_page (
 idpage BIGINT,
@@ -12,11 +8,6 @@ totalcount BIGINT
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
 LOCATION '/piwik/totalcountpage';
 
-#hadoop jar target/convert-page-1.0-SNAPSHOT.jar com.piwik.convertpage.ConvertPageDriver /piwik/joinsinglepathsbyconversion /piwik/pageconversion
-
-#########################
-# piwik_page_conversion #
-#########################
 drop table piwik_page_conversion;
 CREATE EXTERNAL TABLE piwik_page_conversion (
 idpage BIGINT,
